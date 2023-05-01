a00262a601bfde1e180c7ae21721198a40b93731
Briqz23 <dbriquez.devmaua@gmail.com>
2023-04-30 23:32:49 -0300

    populate_Alunosdb.py

--- create.py
+++ create.py
@@ -1,12 +1,19 @@
 import sqlite3
+from populate_Alunosdb import tupla_alunos
+
 
 #cur é cursor e con é connection
 con = sqlite3.connect("DB_notebooks.db")
 cur = con.cursor()
 
 cur.execute("DROP TABLE Aluno");
+cur.execute("DROP TABLE AlunoNotebook");
+cur.execute("DROP TABLE Notebook");
+cur.execute("DROP TABLE Funcionario");
+
+
 cur.execute("CREATE TABLE Aluno("
-            "ra INTEGER NOT NULL PRIMARY KEY,"
+            "ra TEXT NOT NULL PRIMARY KEY,"
             "nome TEXT,"
             "senha TEXT)")
 
@@ -22,3 +29,15 @@ cur.execute("CREATE TABLE AlunoNotebook("
             "FOREIGN KEY(idNotebook) REFERENCES Notebook(idNotebook))")
 
 cur.execute("CREATE TABLE Funcionario (idFuncionario INTEGER NOT NULL PRIMARY KEY, nome TEXT, senha TEXT)")
+
+cur.executemany("insert into Aluno values (?, ?, ?)", tupla_alunos)
+
+con.commit()
+#Checar DB:
+
+cur.execute("SELECT * from Aluno")
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
