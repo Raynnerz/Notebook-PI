f946c3cf91339fe64a49e30ef8326b36aa622505
briqz23 <danielbriquez2@gmail.com>
2023-05-31 12:50:00 -0300

    historico, pendencias e pedidos funcional

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+#autual
 import sqlite3
 from flask import Flask, jsonify, render_template, redirect, url_for, request, session
 import time
@@ -23,7 +24,12 @@ def home_funcionario():
 
 @app.route('/update_requests_devolver')
 def tela_aluno_devolver():
-    return render_template('telaAlunoDevolver.html', username=session['user_id'])
+    if 'user_id' in session:
+        session['username'] = session['user_id']
+        return render_template('telaAlunoDevolver.html', username=session['username'])
+    else:
+        return "Unauthorized access"
+
 
 
 
@@ -45,6 +51,7 @@ def update_requests_devolver():
         return "Request updated successfully"
     else:
         return "Unauthorized access"
+    
     
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -130,7 +137,7 @@ def get_pendencies():
     conn = sqlite3.connect('src/database/DB_notebooks.db')
     cursor = conn.cursor()
 
-    query = "SELECT * FROM AlunoNotebook WHERE DataRetirada IS NOT NULL"
+    query = "SELECT * FROM AlunoNotebook WHERE DataRetirada IS NOT NULL AND request = 0"
     cursor.execute(query)
 
     requests = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
@@ -145,7 +152,21 @@ def get_requests_admin():
     conn = sqlite3.connect('src/database/DB_notebooks.db')
     cursor = conn.cursor()
 
-    query = "SELECT * FROM AlunoNotebook WHERE request = 1 AND DataRetirada IS NULL"
+    query = "SELECT * FROM AlunoNotebook WHERE request = 1 AND DataDevolucao IS NULL"
+    cursor.execute(query)
+
+    requests = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
+    conn.close()
+
+    return jsonify(requests)
+
+#aba pedidos
+@app.route('/get_historico_admin', methods=['GET'])
+def get_historico_admin():
+    conn = sqlite3.connect('src/database/DB_notebooks.db')
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM AlunoNotebook WHERE request = 1 AND DataDevolucao IS NOT NULL"
     cursor.execute(query)
 
     requests = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
@@ -153,30 +174,76 @@ def get_requests_admin():
 
     return jsonify(requests)
 #inserir info no BD com data
+
+""" 
+@app.route('/update_request', methods=['POST'])
+def update_request():
+    request_id = request.form.get('requestId')
+
+    conn = sqlite3.connect('src/database/DB_notebooks.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM AlunoNotebook WHERE idAlunoNotebook = ?", (request_id,))
+    row = cursor.fetchone()
+
+    if row:
+        status = [6]
+        data_retirada = row[4]
+        data_devolucao = row[5]
+
+    if status == '0':
+        if data_retirada != None and data_devolucao == None: 
+            current_time = int(time.time())  
+            query = "UPDATE AlunoNotebook SET request = 0, DataRetirada = ? WHERE idAlunoNotebook = ?"
+            cursor.execute(query, (current_time, request_id))
+            conn.commit()
+            conn.close()
+        elif data_retirada!= None and data_devolucao != None:
+            current_time = int(time.time())  
+            query = "UPDATE AlunoNotebook SET request = 0, DataDevolucao = ? WHERE idAlunoNotebook = ?"
+            cursor.execute(query, (current_time, request_id))
+            conn.commit()
+            conn.close()
+
+
+
+    return 'Request updated successfully'
+"""
+
 @app.route('/update_request', methods=['POST'])
 def update_request():
     request_id = request.form.get('requestId')
     status = request.form.get('status')
 
-
+    print(request_id, status)
     conn = sqlite3.connect('src/database/DB_notebooks.db')
     cursor = conn.cursor()
 
-    if status == '0':
-        current_time = int(time.time())  
-        query = "UPDATE AlunoNotebook SET request = ?, DataRetirada = ? WHERE idAlunoNotebook = ?"
+    cursor.execute("SELECT * FROM AlunoNotebook WHERE idAlunoNotebook = ?", (request_id,))
+    row = cursor.fetchone()
+
+    if row:
+        data_retirada = row[4]
+        data_devolucao = row[5]
+
+        print(f'status: {status}\n ,data_devolucao: {data_devolucao}\n, data_retirada: {data_retirada}')
+        
+    if data_retirada == None:
+        current_time = int(time.time())   
+        query = "UPDATE AlunoNotebook SET request = ?, dataRetirada = ? WHERE idAlunoNotebook = ?"
         cursor.execute(query, (status, current_time, request_id))
-    else:
-        query = "UPDATE AlunoNotebook SET request = ? WHERE idAlunoNotebook = ?"
-        cursor.execute(query, (status, request_id))
+
+    else: 
+        current_time = int(time.time())  
+        query = "UPDATE AlunoNotebook SET request = ?, dataDevolucao = ? WHERE idAlunoNotebook = ?"
+        cursor.execute(query, (status, current_time, request_id))
+
 
     conn.commit()
     conn.close()
 
 
     return 'Request updated successfully'
-
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', threaded=True, debug=True)
